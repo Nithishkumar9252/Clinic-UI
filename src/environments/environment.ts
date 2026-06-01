bc3b5ca881ab0bd6740237cbadc22fd3a48bad81
Nithishkumar9252 <nithishgundla7@gmail.com>
2026-06-01 15:38:17 +0530

    Add Groq key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,7 @@ export const environment = {
 
   apiUrl: 'https://clinic-backend-dty3.onrender.com',
 
-   GROQ_API_KEY:
-
-    '',
+  GROQ_API_KEY: 'YOUR_GROQ_API_KEY',
 
 
   GROQ_API_URL: 'https://api.groq.com/openai/v1'
