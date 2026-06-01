1f96e2ed0ecac1a3aff20ccedebb33f6baa9c3a1
Nithishkumar9252 <nithishgundla7@gmail.com>
2026-06-01 14:55:10 +0530

    Updated production environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,12 @@
 export const environment = {
+  production: true,
 
-  production: false,
+  apiUrl: 'https://clinic-backend-dty3.onrender.com',
 
-
-  GROQ_API_KEY:
+   GROQ_API_KEY:
 
     '',
 
-  GROQ_API_URL:
 
-    ''
-
+  GROQ_API_URL: 'https://api.groq.com/openai/v1'
 };
