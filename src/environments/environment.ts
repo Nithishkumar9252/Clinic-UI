9834f89021efa83ebf23034191cc5609bd1c6a11
Nithishkumar9252 <nithishgundla7@gmail.com>
2026-06-04 18:38:01 +0530

    AI updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
 
   apiUrl: 'https://clinic-backend-dty3.onrender.com',
 
-  GROQ_API_KEY: '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',
+  GROQ_API_KEY: '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',
 
 
   GROQ_API_URL: 'https://api.groq.com/openai/v1'
