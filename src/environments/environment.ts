8b9fee3dab85b165ce7735d3bcb8b43be8089a89
Nithishkumar9252 <nithishgundla7@gmail.com>
2026-06-08 13:37:46 +0530

    key change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ export const environment = {
 
   apiUrl: 'https://clinic-backend-dty3.onrender.com',
 
-  GROQ_API_KEY: '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',
+  GROQ_API_KEY: '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',
 
 
   GROQ_API_URL: 'https://api.groq.com/openai/v1'
